af2ef5e57af6cde8d36c8f7f274b23d7dac67a03
Roy Langa Jr <roy@Roys-MacBook-Pro.local>
2024-04-16 02:42:46 +0200

    Created a connectivity plugin and added necesarry code to check network connectivity, facing an MissingPluginException error, will continue investigating

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,27 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:connectivity_plugin/connectivity_plugin.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ConnectivityExampleScreen(),
+      title: 'Littlefish Assessment',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
 
-class ConnectivityExampleScreen extends StatefulWidget {
-  @override
-  _ConnectivityExampleScreenState createState() => _ConnectivityExampleScreenState();
-}
 
-class _ConnectivityExampleScreenState extends State<ConnectivityExampleScreen> {
-  bool _isConnected = true; // Default assumption: connected to the internet
-  late StreamSubscription<bool> _connectivitySubscription;
 
-  @override
-  void initState() {
-    super.initState();
-    // Check current connectivity status
-    ConnectivityPlugin.isConnected().then((isConnected) {
-      setState(() {
-        _isConnected = isConnected;
-      });
-    });
 
-    // Listen to connectivity changes
-    _connectivitySubscription = ConnectivityPlugin.onConnectivityChanged.listen((isConnected) {
-      setState(() {
-        _isConnected = isConnected;
-      });
-    });
-  }
 
-  @override
-  void dispose() {
-    // Cancel the subscription to avoid memory leaks
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Connectivity Example'),
-      ),
-      body: Center(
-        child: Text(
-          _isConnected ? 'Connected to the internet' : 'No internet connection',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
-  }
-}
